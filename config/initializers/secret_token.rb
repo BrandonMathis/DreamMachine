ed12e895dbd79d0e7292f2a236ff86741624558b
BrandonMathis <Brandon@KeysetTS.com>
2011-09-05 01:47:43 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamMachine::Application.config.secret_token = '1f89d10b6b6a3a4898d7a2f9efcc2f7e404f2bf42a0e5ed93b665f69784608ec216a6b20a55dde951f7115667f1e3817c99c7182a7444732e674843aef682517'
