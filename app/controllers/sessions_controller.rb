05bc1ec4ae0377f010a233721bc9fc5a6590202c
BrandonMathis <Brandon@KeysetTS.com>
2011-09-13 17:27:05 -0400

    User login ignorecase

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = login(params[:username], params[:password], params[:remember_me])
+    user = login(params[:username].downcase, params[:password], params[:remember_me])
     if user
       redirect_back_or_to root_url, :notice => "Logged In!"
     else
